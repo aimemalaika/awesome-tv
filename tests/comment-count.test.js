9132644d47a6ec6803b8b7c626158d070e5fbc22
oyelakinG9 <oyelakinr37@gmail.com>
2022-02-03 21:53:25 +0100

    Add comment-count file

---
+++ tests/comment-count.test.js
@@ -0,0 +1,23 @@
+import Comment from '../src/component/Comment.js';
+​
+const unmockedFetch = global.fetch;
+const postId = Date.now();
+const comment = new Comment('User 1', 'message 1', postId);
+beforeAll(async () => {
+  global.fetch = () => Promise.resolve({
+    json: () => Promise.resolve([JSON.stringify(comment)]),
+  });
+  await Comment.postComment();
+});
+​
+afterAll(() => {
+  global.fetch = unmockedFetch;
+});
+​
+describe('Comment class', () => {
+  document.body.innerHTML = '<p class="movies-counter"></p>';
+  test('return the number of comments of a shows', async () => {
+    const allComment = await Comment.getComment(postId);
+    expect(allComment.length).toBe(1);
+  });
+});
